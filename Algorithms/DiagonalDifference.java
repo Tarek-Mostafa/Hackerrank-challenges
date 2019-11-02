5b8f46d19e87ce1d6a6a261bd071188bf0f2f558
Tarek Mostafa <Tarek-Mostafa@users.noreply.github.com>
2019-11-02 13:17:16 +0200

    Update DiagonalDifference.java

--- Algorithms/DiagonalDifference.java
+++ Algorithms/DiagonalDifference.java
@@ -1,1 +1,40 @@
 
+/*
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ *
+ * Link to challenge: https://www.hackerrank.com/challenges/diagonal-difference/problem
+ */
+ 
+import java.lang.*;
+
+public class DiagonalDiffernce
+{
+    public static void main(String[] args) {
+        testDiagonalDiff();
+    }
+    
+    public static int getDiagonalDifference() {
+          int arr[][] = {
+          {11,2,4},
+          {4,5,6},
+          {10,8,-12}
+        };
+        
+        int LTR = 0;
+        int RTL = 0;
+        int diagonalDiffernce = 0;
+        
+        for(int i = 0; i<arr.length; i++) {
+            LTR += arr[i][i];
+            RTL += arr[i][arr.length - 1 - i];
+        }
+        
+        diagonalDiffernce = Math.abs(LTR-RTL);
+        return(diagonalDiffernce);
+    }
+    
+    public static void testDiagonalDiff() {
+        int x = getDiagonalDifference();
+        System.out.println(x);
+    }
+}
