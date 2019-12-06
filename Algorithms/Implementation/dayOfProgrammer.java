568641c8042f3682d4e7a16f999eac2e8509c508
Tarek Mostafa <Tarek-Mostafa@users.noreply.github.com>
2019-12-06 07:45:35 +0200

    Create dayOfProgrammer.java

---
+++ Algorithms/Implementation/dayOfProgrammer.java
@@ -0,0 +1,17 @@
+/* Link to challenge: https://www.hackerrank.com/challenges/day-of-the-programmer/problem */
+
+static String dayOfProgrammer(int year) {
+  if(year == 1918) {
+    return "26.09.1918";
+  }
+
+  if(year < 1918 && year % 4 == 0) {
+    return "12.09." + year;
+  } 
+  else if(  year > 1918 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0 ))  ) {
+    return "12.09." + year;
+  } 
+  else {
+    return "13.09." + year;
+  }
+}
