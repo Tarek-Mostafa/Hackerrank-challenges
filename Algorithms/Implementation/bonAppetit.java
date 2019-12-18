b0ed2263f55c44733d2dc0585da3a4593976bc70
Tarek Mostafa <Tarek-Mostafa@users.noreply.github.com>
2019-12-18 06:00:39 +0200

    Create bonAppetit.java

---
+++ Algorithms/Implementation/bonAppetit.java
@@ -0,0 +1,13 @@
+static void bonAppetit(List<Integer> bill, int k, int b) {
+  int amountToBePainByAnna = 0;
+  for(int i = 0; i < bill.size(); i++) {
+    if(i == k)
+      continue;
+    amountToBePainByAnna += bill.get(i);
+  }
+  amountToBePainByAnna /= 2;
+  if (amountToBePainByAnna == b) 
+    System.out.println("Bon Appetit");
+  else
+    System.out.println(b - amountToBePainByAnna);
+}
